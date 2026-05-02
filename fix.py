dc4c8d9a74ec94227c12faa9894609290939f3fe
bengalebg23 <ben.gale@hotmail.co.uk>
2026-05-02 18:53:14 +0100

    update v3.1.8

--- fix.py
+++ fix.py
@@ -2,12 +2,19 @@ path = '/data/data/com.termux/files/home/meal-planner/index.html'
 with open(path, 'r') as f:
     content = f.read()
 
-old_v = 'v3.1.6'
-new_v = 'v3.1.7'
+# Show offset in date for debugging
+old = "  return d.getDate() + '/' + (d.getMonth()+1);"
+new = "  return d.getDate() + '/' + (d.getMonth()+1) + '(' + getWeekOffset() + ')';"
 
-content = content.replace(old_v, new_v)
-content = content.replace('meal-planner-v3.1.6', 'meal-planner-v3.1.7')
+old_v = 'v3.1.7'
+new_v = 'v3.1.8'
 
-with open(path, 'w') as f:
-    f.write(content)
-print("Done")
+if old in content:
+    content = content.replace(old, new)
+    content = content.replace(old_v, new_v)
+    content = content.replace('meal-planner-v3.1.7', 'meal-planner-v3.1.8')
+    with open(path, 'w') as f:
+        f.write(content)
+    print("Done")
+else:
+    print("MISS")
